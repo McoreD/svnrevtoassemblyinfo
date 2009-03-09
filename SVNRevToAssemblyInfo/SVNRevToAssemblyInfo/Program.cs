0c995160d4aa821c71dbd801ef8ec2df13be44f1
mcored <mcored@e727dea2-0c83-11de-b8f3-b9fb9eb71b04>
2009-03-09 08:47:18 +0000

    Using LibSubWCRev to get latest SVN Revision number

---
+++ SVNRevToAssemblyInfo/SVNRevToAssemblyInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVNRevToAssemblyInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working folder: " + args[0]);
+            LibSubWCRev.SubWCRevClass svn = new LibSubWCRev.SubWCRevClass();
+            svn.GetWCInfo(args[0], true, true);
+            Console.WriteLine("Latest Revision: " + svn.Revision);
+            Console.WriteLine("Last Author: " + svn.Author);
+            Console.WriteLine("Last Commited Date: " + svn.Date);
+            Console.WriteLine("SVN: " + svn.Url);
+          
+            Console.ReadLine();
+        }
+    }
+}
