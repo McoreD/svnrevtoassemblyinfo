ccda4015dd9785d7f76ff9bd12528dde1cc06094
mcored <mcored@e727dea2-0c83-11de-b8f3-b9fb9eb71b04>
2009-03-10 09:25:17 +0000

    Using CommandLlineParserLib for successful Pre-build in VS2008

--- SVNRevToAssemblyInfo/SVNRevToAssemblyInfo/Program.cs
+++ SVNRevToAssemblyInfo/SVNRevToAssemblyInfo/Program.cs
@@ -4,79 +4,91 @@ using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
 using NDesk.Options;
+using CommandLineParserLib;
+using System.Reflection;
 
 namespace SVNRevToAssemblyInfo
 {
     class Program
     {
+        const string FLAG_WORKING_DIR = "dir";
+        const string FLAG_ASSEMBLY_INFO = "file";
 
+        static string GetCommandLine()
+        {
+            string cli = Environment.CommandLine;
+            cli = cli.Replace(Path.GetFileName(Assembly.GetExecutingAssembly().Location), "").Trim();
+            cli = cli.Replace("\"" + Path.GetFileName(Assembly.GetExecutingAssembly().Location) + "\"", "").Trim();
+            cli = cli.Replace("\"" + Assembly.GetExecutingAssembly().Location + "\"", "").Trim();
+            cli = cli.Replace("\"" + Assembly.GetExecutingAssembly().Location.Replace(".exe", ".vshost.exe") + "\"", "").Trim();
+
+            return cli;
+        }
         static void Main(string[] args)
         {
             // args[0] is the trunk folder 
             // args[1] is the file path of AssemblyInfo.cs or AssemblyInfo.vb
 
-            StringBuilder sbDebug = new StringBuilder();
-            sbDebug.AppendLine("Command Line: " + Environment.CommandLine);
-
-            bool bShowHelp = false;
             string pWorkingdir = "";
-            string fAssemblyInfo = "";
-            bool bWriteDebug = false;
-   
-            var p = new OptionSet() {
-                { "i|input=", "Working folder path such as the trunk folder.",
-                  (string v) => pWorkingdir = v },
-                { "o|output=", "Assemblyinfo file path.",
-                  (string v) => fAssemblyInfo = v },
-                { "v|verbose", "Verbose Mode",
-                  v => { if (v != null) bWriteDebug = true; } },
-                { "h|help",  "show this message and exit", 
-                  v => bShowHelp = v != null },
-            };
+            string pAssemblyInfo = "";
 
-            List<string> extra;
-            try
-            {
-                extra = p.Parse(args);
-            }
-            catch (OptionException e)
-            {
-                Console.Write("SVNRevToAssembyInfo: ");
-                Console.WriteLine(e.Message);
-                Console.WriteLine("Try SVNRevToAssembyInfo.exe --help' for more information.");
-                return;
-            }
+            StringBuilder sbDebug = new StringBuilder();
+          
 
-            if (bShowHelp)
-            {
-                ShowHelp(p);
-                return;
-            }
+            CommandLineParser parser = new CommandLineParser();
+            parser.CommandLine = GetCommandLine();
+            sbDebug.AppendLine("Command Line: " + parser.CommandLine);
 
-            if (extra.Count > 0)
+            CommandLineEntry dirArg = parser.CreateEntry(CommandTypeEnum.Flag, FLAG_WORKING_DIR);
+            parser.Entries.Add(dirArg);
+            CommandLineEntry dirVal = dirArg;
+            dirArg = parser.CreateEntry(CommandTypeEnum.ExistingFolder);
+            dirArg.MustFollowEntry = dirVal;
+            parser.Entries.Add(dirArg);
+
+            CommandLineEntry fileArg = parser.CreateEntry(CommandTypeEnum.Flag, FLAG_ASSEMBLY_INFO);
+            parser.Entries.Add(fileArg);
+            CommandLineEntry fileVal = fileArg;
+            fileArg = parser.CreateEntry(CommandTypeEnum.ExistingFile);
+            fileArg.MustFollowEntry = fileVal;
+            parser.Entries.Add(fileArg);
+
+            if (parser.Parse())
             {
-                for (int i = 0; i < extra.Count; i++)
+                for (int i = 0; i < parser.Entries.Count; i++)
                 {
-                    sbDebug.AppendLine(string.Format("Arg {0} {1}", i, extra[i]));
+                    CommandLineEntry entry = parser.Entries.get_Item(i);
+                    if (entry.Value.Equals(FLAG_WORKING_DIR))
+                    {
+                        if (parser.Entries.Count > 1)
+                        {
+                            pWorkingdir = parser.Entries.get_Item(++i).Value;
+                        }
+                    }
+                    else if (entry.Value.Equals(FLAG_ASSEMBLY_INFO))
+                    {
+                        if (parser.Entries.Count > 1)
+                        {
+                            pAssemblyInfo = parser.Entries.get_Item(++i).Value;
+                        }
+                    }
                 }
             }
 
-            sbDebug.AppendLine(Environment.NewLine);
-
-            if (args.Length == 2)
+            if (!string.IsNullOrEmpty(pWorkingdir) && !string.IsNullOrEmpty(pAssemblyInfo))
             {
-                sbDebug.AppendLine("Working folder: " + args[0]);
+                sbDebug.AppendLine("Working folder: " + pWorkingdir);
                 LibSubWCRev.SubWCRevClass svn = new LibSubWCRev.SubWCRevClass();
-                svn.GetWCInfo(args[0], true, true);
+                svn.GetWCInfo(pWorkingdir, true, true);
                 sbDebug.AppendLine("Latest Revision: " + svn.Revision);
                 sbDebug.AppendLine("Last Author: " + svn.Author);
                 sbDebug.AppendLine("Last Commited Date: " + svn.Date);
                 sbDebug.AppendLine("SVN: " + svn.Url);
 
                 string ai = "";
-                using (StreamReader sr = new StreamReader(args[1]))
+                using (StreamReader sr = new StreamReader(pAssemblyInfo))
                 {
-                    sbDebug.AppendLine("Reading: " + args[1]);
+                    sbDebug.AppendLine("Reading: " + pAssemblyInfo);
                     ai = sr.ReadToEnd();
                     string version = "AssemblyVersion"; //AssemblyFileVersion
                     int rev = 0;
@@ -86,9 +98,9 @@ namespace SVNRevToAssemblyInfo
                 }
                 if (!string.IsNullOrEmpty(ai))
                 {
-                    using (StreamWriter sw = new StreamWriter(args[1]))
+                    using (StreamWriter sw = new StreamWriter(pAssemblyInfo))
                     {
-                        sbDebug.AppendLine("Writing: " + args[1]);
+                        sbDebug.AppendLine("Writing: " + pAssemblyInfo);
                         sw.WriteLine(ai);
                         sbDebug.AppendLine(Environment.NewLine);
                         sbDebug.AppendLine(ai);
@@ -96,30 +108,13 @@ namespace SVNRevToAssemblyInfo
                 }
             }
 
+            Console.WriteLine(sbDebug.ToString());
+
             using (StreamWriter sw = new StreamWriter("debug.log"))
             {
                 sw.WriteLine(sbDebug.ToString());
             }
 
         }
-
-        static void ShowHelp(OptionSet p)
-        {
-            Console.WriteLine("Usage: greet [OPTIONS]+ message");
-            Console.WriteLine("Greet a list of individuals with an optional message.");
-            Console.WriteLine("If no message is specified, a generic greeting is used.");
-            Console.WriteLine();
-            Console.WriteLine("Options:");
-            p.WriteOptionDescriptions(Console.Out);
-        }
-
-        //static void Debug(string format, params object[] args)
-        //{
-        //    if (verbosity > 0)
-        //    {
-        //        Console.Write("# ");
-        //        Console.WriteLine(format, args);
-        //    }
-        //}
     }
 }
